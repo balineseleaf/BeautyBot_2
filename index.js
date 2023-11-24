e816f646edf02c530c0c710fb159c84b1e07e837
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-11-24 19:09:32 +0300

    add all html templates for main menu buttons

--- index.js
+++ index.js
@@ -3,17 +3,31 @@
 //import Api from './components/Api.js';
 
 let tg = window.Telegram.WebApp; // создаем объект телеграмма
-
 tg.expand();
-tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = '#0a85d8';
 
-// tg.BackButton.isVisible(false);
+// tg.MainButton.textColor = '#FFFFFF';
+// tg.MainButton.color = '#0a85d8';
+
+// let currentPage = window.location.href;
+
+// if (currentPage !== 'http://127.0.0.1:5500/index.html') {
+//   tg.BackButton.show();
+//   tg.onEvent('backButtonClicked', function () {
+//     window.history.back();
+//   });
+// } else {
+//   tg.BackButton.hide();
+// }
 tg.BackButton.show();
 tg.onEvent('backButtonClicked', function () {
   window.history.back();
   tg.BackButton.hide();
   // tg.close();
+});
+
+const closeButton = document.getElementById('close-btn');
+closeButton.addEventListener('click', function () {
+  window.history.back();
 });
 
 // p.innerText = `${tg.initDataUnsafe.user.first_name}
