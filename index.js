6af9ba1e60fe07d073a963fa4731f0e2c61abf8a
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-11-24 08:26:06 +0300

    add all html templates for main menu buttons

--- index.js
+++ index.js
@@ -1,52 +1,22 @@
-import PopupWithForm from './components/PopupWithForm.js';
-import UserInfo from './components/UserInfo.js';
+// import PopupWithForm from './components/PopupWithForm.js';
+// import UserInfo from './components/UserInfo.js';
 //import Api from './components/Api.js';
 
 let tg = window.Telegram.WebApp; // создаем объект телеграмма
 
-tg.expand(); // на весь экран сайт
-
-// создается тг, а не сайтом эта кнопка
-tg.MainButton.textColor = '#FFFFFF'; // изменим текст кнопки
+tg.expand();
+tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#0a85d8';
-let item = '';
 
-let btn1 = document.getElementById('btn1');
-let btn2 = document.getElementById('btn2');
-
-btn1.addEventListener('click', function () {
-  if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-  } else {
-    tg.MainButton.setText('Вы выбрали товар 1!');
-    item = '1';
-    tg.MainButton.show();
-  }
+tg.BackButton.show();
+tg.onEvent('backButtonClicked', function () {
+  console.log('cryptoShagan');
+  window.history.back();
 });
+// p.innerText = `${tg.initDataUnsafe.user.first_name}
+// ${tg.initDataUnsafe.user.last_name}`;
+// usercard.appendChild(p); // вставляем параграф с данными пользователя
 
-btn2.addEventListener('click', function () {
-  if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-  } else {
-    tg.MainButton.setText('Вы выбрали товар 2!');
-    item = '2';
-    tg.MainButton.show();
-  }
-});
-
-// Функция, которая передаст данные на сайт
-// Telegram.WebApp.onEvent('mainButtonClicked', function () {
-//   tg.sendData(item); // отправляем данные
-// });
-
-Telegram.WebApp.onEvent('mainButtonClicked', function () {
-  tg.MainButton.textColor = '#000000';
-});
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-usercard.appendChild(p); // вставляем параграф с данными пользователя
-
-// Telegram.WebApp.onEvent('mainButtonClicked', function () {
+// Telegram.WebApp.onClick('mainButtonClicked', function () {
 //   tg.sendData(); // отправляем данные
 // });
